67c98b46a10a1ec3152a9b70348050333c0fa91c
Max <scorpiomax90@gmail.com>
2026-01-28 11:17:01 +0200

    Create sliders array

--- src/components/SliderCarousel/SliderCarouselItem.vue
+++ src/components/SliderCarousel/SliderCarouselItem.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="carousel__item">{{ slide }}</div>
+  <div class="carousel__item">
+    <h2>{{ slide.title }}</h2>
+
+    <ul v-if="slide.bullets">
+      <li v-for="item in slide.bullets" :key="item">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/components/SliderCarousel/SliderCarouselWrapper.vue
+++ src/components/SliderCarousel/SliderCarouselWrapper.vue
@@ -1,6 +1,6 @@
 <template>
   <Carousel v-bind="carouselConfig">
-    <Slide v-for="slide in 10" :key="slide">
+    <Slide v-for="slide in slideStore.slides" :key="slide.id">
       <SliderCarouselItem :slide="slide" />
     </Slide>
 
@@ -14,7 +14,10 @@
 <script setup lang="ts">
 import 'vue3-carousel/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { useSlideStore } from '@/stores/slider'
 import SliderCarouselItem from './SliderCarouselItem.vue'
+
+const slideStore = useSlideStore()
 
 const carouselConfig = {
   itemsToShow: 2.5,

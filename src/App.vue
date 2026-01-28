bd4f2f6d26e3af7258e1820ed8d8d2ff16cf275e
Max <scorpiomax90@gmail.com>
2026-01-28 10:38:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
+
+<style scoped></style>
